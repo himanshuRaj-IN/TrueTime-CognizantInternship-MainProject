1f7e4296a063b24d635f03c815a7fba998d083f1
Himanshu Raj <himanshuraj9194@gmail.com>
2022-04-08 17:29:54 +0530

    cleaned file for main Project

---
+++ src/main/java/pages/SignInPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import com.aventstack.extentreports.ExtentTest;
+import BaseClasses.PagesBaseClass;
+
+public class SignInPage extends PagesBaseClass {
+
+	public SignInPage(WebDriver driver, ExtentTest logger) {
+		super(driver, logger);
+	}
+
+}
---
+++ src/main/java/pages/TrueTimePage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import com.aventstack.extentreports.ExtentTest;
+import BaseClasses.PagesBaseClass;
+
+public class TrueTimePage extends PagesBaseClass{
+
+	public TrueTimePage(WebDriver driver, ExtentTest logger) {
+		super(driver, logger);
+	}
+
+}
